dd394248788bddf1749910090867e396971b91e5
Ehmcke <68448754+Ehmcke@users.noreply.github.com>
2024-04-26 13:03:37 -0700

    updated function in update script

--- os_upgrade.sh
+++ os_upgrade.sh
@@ -1,19 +1,29 @@
-#!/bin/bash
+ #!/bin/bash
 
-release_file=/etc/os-release 
+release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
+
+check_exit_status() {
+        if [ $? -ne 0 ]
+        then
+                echo "An error occurred, please check the $errorlog file."
+        fi
+}
 
 if grep -q "Arch" $release_file
 then
         # The host is based on Arch, run the pacman update command
-        sudo pacman -Syu
+        sudo pacman -Syu 1>>$logfile 2>>$errorlog
+        check_exit_status
 fi
 
 if grep -q -e "Ubuntu" -e "Debian" $release_file
 then
         # The host is based on Debian or Ubuntu,
         # Run the apt version of the command
-        sudo apt upgrade
-        sudo apt dist-upgrade
+        sudo apt upgrade 1>>$logfile 2>>$errorlog
+        check_exit_status
+        sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
+        check_exit_status
 fi
-
-
